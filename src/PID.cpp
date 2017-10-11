da6378a2b34232627f58311d49327e04454a99a2
root <root@DESKTOP-VS29QMA.localdomain>
2017-10-11 12:00:07 -0700

    initial PID implementation, car not stable at throttle 0.3

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,25 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+    // note this -> var is anologous to self.var = var in python
+    this -> Kp = Kp;
+    this -> Ki = Ki;
+    this -> Kd = Kd;
+
+    double p_error = 0, i_error = 0, d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+   // TODO Add link to Sebastion lecure and make sure this is consitent
+   p_error = cte;
+   i_error += cte;
+   d_error = cte - p_error;
 }
 
 double PID::TotalError() {
+
+    return Kp * p_error + Ki * i_error + Kd * d_error;
+
 }
 
